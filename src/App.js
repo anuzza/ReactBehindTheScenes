58b719639421b6e4380c273cda80a4988f6e50dd
anuzza <anuja3580@gmail.com>
2022-06-15 03:15:44 -0500

    analyzing state changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import './App.css';
+import Button from './Components/UI/Button/Button';
 
 function App() {
+  const [btnClicked, setBtnClicked]= useState(false);
+
+  const onClickHandler = ()=>{
+    setBtnClicked((prevShow)=>!prevShow);
+
+  }
+  
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {btnClicked && <p>This is new!</p>}
+      <Button onClick = {onClickHandler}>Show paragraph! </Button>
     </div>
   );
 }
